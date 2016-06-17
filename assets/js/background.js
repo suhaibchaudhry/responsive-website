f4284bb4879878783c19ffc767bf6fb79c8d8340
Asad Hasan <asad@uitoux.com>
2016-06-17 13:17:03 -0400

    Background conditional

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,3 +1,7 @@
+(function($) {
+if($('#background-animation').length > 0) {
 particlesJS.load('background-animation', '/configs/particlesjs-config.json', function() {
   //console.log('callback - particles.js config loaded');
 });
+}
+})(jQuery);
